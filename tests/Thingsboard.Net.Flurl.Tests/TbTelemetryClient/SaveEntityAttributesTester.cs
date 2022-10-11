bf94321ae3603656d69b696ae770b3d57e9d189a
Nepton <nepton@qq.com>
2022-10-11 18:15:52 +0800

    add unit test

---
+++ tests/Thingsboard.Net.Flurl.Tests/TbTelemetryClient/SaveEntityAttributesTester.cs
@@ -0,0 +1,24 @@
+﻿using Thingsboard.Net.Exceptions;
+
+namespace Thingsboard.Net.Tests.TbTelemetryClient;
+
+public class SaveEntityAttributesTester
+{
+    [Fact]
+    public async Task TestSaveEntityAttributes()
+    {
+        // arrange
+        using var service  = new TbTestService();
+        var       api      = service.GetRequiredService<ITbTelemetryClient>();
+        var       deviceId = service.GetTestDeviceId();
+
+        // act
+        var ex = await Record.ExceptionAsync(async () =>
+        {
+            await api.SaveEntityAttributesAsync(TbEntityType.DEVICE, deviceId, TbAttributeScope.CLIENT_SCOPE, new Dictionary<string, string> {{"key", "value"}});
+        });
+
+        // act
+        Assert.IsType<TbHttpException>(ex);
+    }
+}
