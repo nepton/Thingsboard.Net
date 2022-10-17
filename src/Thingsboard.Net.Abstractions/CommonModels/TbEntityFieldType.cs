f2ae0c9ba37e7ed2a5baea792f50adaac9043920
Nepton <nepton@qq.com>
2022-10-17 09:56:24 +0800

    fine tuning with project

--- src/Thingsboard.Net.Abstractions/CommonModels/TbEntityFieldType.cs
+++ src/Thingsboard.Net.Abstractions/CommonModels/TbEntityFieldType.cs
@@ -1,12 +1,42 @@
 ﻿namespace Thingsboard.Net;
 
+/// <summary>
+/// Entity field type.
+/// </summary>
 public enum TbEntityFieldType
 {
+    /// <summary>
+    /// CLIENT_ATTRIBUTE 
+    /// </summary>
     CLIENT_ATTRIBUTE,
+
+    /// <summary>
+    ///SHARED_ATTRIBUTE
+    /// </summary>
     SHARED_ATTRIBUTE,
+
+    /// <summary>
+    ///SERVER_ATTRIBUTE
+    /// </summary>
     SERVER_ATTRIBUTE,
+
+    /// <summary>
+    ///ATTRIBUTE
+    /// </summary>
     ATTRIBUTE,
+
+    /// <summary>
+    ///TIME_SERIES
+    /// </summary>
     TIME_SERIES,
+
+    /// <summary>
+    ///ENTITY_FIELD
+    /// </summary>
     ENTITY_FIELD,
+
+    /// <summary>
+    ///ALARM_FIELD
+    /// </summary>
     ALARM_FIELD,
 }
